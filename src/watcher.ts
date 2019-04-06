7bd20322020e8a73a3e2371198957649b2ed8c2f
Leko <leko.noor@gmail.com>
2019-04-06 23:18:41 +0900

    Create Watcher and watch

---
+++ src/watcher.ts
@@ -0,0 +1,26 @@
+import { FSWatcher, WatchOptions } from "chokidar";
+import ignore from "ignore";
+
+type Ignore = ReturnType<typeof ignore>;
+
+type Options = {
+  ignore: Ignore;
+};
+
+export class Watcher extends FSWatcher {
+  private ignore: Ignore;
+
+  constructor(opts: WatchOptions & Options) {
+    const { ignore, ...options } = opts;
+    super(options);
+    this.ignore = ignore;
+  }
+
+  _isIgnored(path: string): boolean {
+    return this.ignore.ignores(path);
+  }
+}
+
+export function watch(dir: string, options: WatchOptions & Options) {
+  return new Watcher(options).add(dir);
+}
