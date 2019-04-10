20916eec45a2b29d2a9257d9f971caac6936c10c
Leko <leko.noor@gmail.com>
2019-04-10 13:04:17 +0900

    test: add tests for wordWrap

--- src/lib/ansi.test.ts
+++ src/lib/ansi.test.ts
@@ -1,10 +1,18 @@
 import assert from "assert";
-import { head } from "./ansi";
+import { head, wordWrap } from "./ansi";
 
 describe("head", () => {
   it("returns string that specified number of lines", () => {
     const expected = "a\n".repeat(4).trim();
     const actual = head("a\n".repeat(5), 4);
+    assert.strictEqual(actual, expected);
+  });
+});
+
+describe("wordWrap", () => {
+  it("returns string that wrapped at specified width", () => {
+    const expected = "a".repeat(5) + "\na";
+    const actual = wordWrap("a".repeat(6), 5);
     assert.strictEqual(actual, expected);
   });
 });
